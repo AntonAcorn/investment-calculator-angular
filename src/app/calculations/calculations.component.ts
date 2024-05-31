cd02b178273fd5c71aa813f6be40bb40bae6ba20
AntonAcorn <ShustikovAnimation@gmail.com>
2024-05-31 07:53:07 -0400

    #Add table

---
+++ src/app/calculations/calculations.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculations',
+  standalone: true,
+  templateUrl: './calculations.component.html',
+	styleUrl: './calculations.component.css'
+})
+export class CalculationsComponent {}
