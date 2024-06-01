5b4a8691dde4096c325467dc8b63243ed894c5f8
AntonAcorn <ShustikovAnimation@gmail.com>
2024-05-31 20:19:02 -0400

    #Add two-way binding

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
   templateUrl: './calculator.component.html',
-	styleUrl: './calculator.component.css'
+	styleUrl: './calculator.component.css',
+	imports:[FormsModule]
 })
-export class CalculatorComponent {}
+export class CalculatorComponent {
+
+	enteredInitialInvestement!: number;
+	enteredAnnualInvestment!: number;
+	
+	onSubmit() {
+		console.log(
+			"Submitted: "  + 
+			this.enteredInitialInvestement,
+			this.enteredAnnualInvestment
+		);
+	}
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-	styleUrl: './header.component.css'
+	styleUrl: './header.component.css',
+	imports: [FormsModule]
 })
 export class HeaderComponent {}
